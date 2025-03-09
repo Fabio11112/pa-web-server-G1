e49be23ed04fc4e31f208449b404ce99c95197c6
[Gabriel2121621] <[2121622@student.uma.pt]>
2025-03-09 11:53:30 +0000

    ci : ServerTest class change

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,17 +1,5 @@
-package hello;
+import org.junit.jupiter.api.*;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class GreeterTest {
-
-    private Greeter greeter = new Greeter();
-
-    @Test
-    public void greeterSaysHello() {
-        assertThat(greeter.sayHello(), containsString("Hello"));
-    }
+public class ServerTest {
 
 }
