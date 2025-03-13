97aacc75e07e6bd03554df06a130023048393640
Fabio11112 <2123422@student.uma.pt>
2025-03-13 10:49:00 +0000

    test: test for LockInitialiser class

---
+++ src/test/java/LockInitialiserTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for LockInitialiser
+ */
+@DisplayName("Lock Initialiser")
+public class LockInitialiserTest {
+
+
+    @Test
+    @DisplayName("Test the correct creation of locks")
+    void testCreateLocks() {
+
+        LockInitialiser lockInitialiser = new LockInitialiser("html");
+        TreeMap<String, Lock> locks = lockInitialiser.createLocks("sites");
+        assertNotNull(locks);
+
+        try (Stream<Path> stream = Files.walk(Paths.get("sites"))) {
+            stream.filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".html"))
+                    .forEach(path -> assertTrue(locks.containsKey(path.toString())));
+        } catch (IOException e) {
+            fail("IOException occurred while listing .html files: " + e.getMessage());
+        }
+    }
+
+
+}
